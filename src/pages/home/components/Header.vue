1a114a554cd3125183630dd50617d678d29e62b7
githubMYL <githubMYL@163.com>
2018-10-27 16:45:14 +0800

    swiper done

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
     <div class="header">
-        <div class="header-left">返回</div>
-        <div class="header-input">输入城市/景点/游玩主题</div>
-        <div class="header-right">城市</div>
+        <div class="header-left">
+            <i class="iconfont">&#xe624;</i>
+        </div>
+        <div class="header-input">
+            <i class="iconfont">&#xe632;</i>
+            输入城市/景点/游玩主题    
+        </div>
+        <div class="header-right">
+            城市
+            <i class="iconfont">&#xe62d;</i>
+        </div>
     </div>
 </template>
 
@@ -13,14 +21,16 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+    @import '~styles/variables.styl'
     .header
         display: flex
         line-height: .86rem
-        background: #00bcd4
+        background: $bgColor
         color: #fff
         .header-left
             width: .64rem
             float: left
+            text-align: center
         .header-input
             flex: 1
             height: .64rem
@@ -29,6 +39,7 @@ export default {
             background: #fff
             border-radius: .1rem
             color: #ccc
+            padding-left: .1rem
         .header-right
             width: 1.24rem;
             float: right
---
+++ src/pages/home/components/swiper.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <!-- slides -->
+            <swiper-slide>
+                <img src = "http://img1.qunarzz.com/piao/fusion/1809/ae/7518c2fbc1415202.jpg_750x200_09bbe391.jpg"/> 
+            </swiper-slide>
+            <swiper-slide>
+                <img src = "http://img1.qunarzz.com/piao/fusion/1810/a8/664024556572fd02.jpg_750x200_ffe41025.jpg"/> 
+            </swiper-slide>
+            <swiper-slide>
+                <img src = "http://img1.qunarzz.com/piao/fusion/1810/e2/2e3774e52b380102.jpg_750x200_14e6ebab.jpg" />
+            </swiper-slide>
+            <!-- Optional controls -->
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper> 
+    </div>  
+</template>
+
+<script>
+export default {
+    name: 'HomeSwiper',
+    data(){
+        return {
+            swiperOption: {
+                pagination: '.swiper-pagination',
+                loop: true
+            }
+        };
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background: #fff
+    .wrapper
+        width: 100%
+        height: 0
+        padding-bottom: 31.25%
+        overflow: hidden
+        img
+            width: 100%    
+</style>
