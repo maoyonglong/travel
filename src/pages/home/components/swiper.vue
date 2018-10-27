f113200231b157a84faccbb993b509ab42296fca
githubMYL <githubMYL@163.com>
2018-10-27 20:00:04 +0800

    解决index-swiper的冲突内容

--- src/pages/home/components/swiper.vue
+++ src/pages/home/components/swiper.vue
@@ -37,7 +37,7 @@ export default {
     .wrapper
         width: 100%
         height: 0
-        padding-bottom: 31.25%
+        padding-bottom: 26.67%
         overflow: hidden
         img
             width: 100%    
