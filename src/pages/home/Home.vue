9ca02e664978f68ef64eeab8ccb6bd67599e95fa
githubMYL <githubMYL@163.com>
2018-10-28 13:25:37 +0800

    完成小工具

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,8 +1,10 @@
 <template>
-    <div>
+    <div class="page">
         <home-header></home-header>
         <home-swiper></home-swiper>
         <home-icons></home-icons>
+        <home-tools></home-tools>
+        <p>123</p>
     </div>
 </template>
 
@@ -10,14 +12,20 @@
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
+import HomeTools from './components/SmallTools'
 export default {
     name: 'home',
     components: {
         HomeHeader,
         HomeSwiper,
-        HomeIcons
+        HomeIcons,
+        HomeTools
     }
 }
 </script>
 
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+    .page{
+        background-color: #f5f5f5
+    }
+</style>
---
+++ src/pages/home/components/SmallTools.vue
@@ -0,0 +1,37 @@
+<template>
+    <ul class="tools-wrapper">
+        <li class="tool">
+            <i class="iconfont">&#xe65d;</i>
+            <span>定位失败</span>
+        </li>
+        <li class="tool">
+            <i class="iconfont">&#xe642;</i>
+            <span>玩水季</span>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'HomeRecommend'
+}
+</script>
+
+<style lang="stylus" scoped>
+    .tools-wrapper
+        display: flex
+        background: #fff
+        margin-top: .1rem
+        margin-bottom: .3rem
+        .tool
+            flex: 1
+            border-top: 1px solid #f0f0f0
+            border-left: 1px solid #f0f0f0
+            text-align: center
+            padding-top: .32rem
+            padding-bottom: .32rem
+            span 
+                color: #212121
+        .tool:nth-child(1)
+            border-left: 0
+</style>
